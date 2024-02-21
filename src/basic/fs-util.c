fc560e210f969c3bee961591f042a1b3ecb13561
Nils Zottmann <n.zottmann@mozys.de>
2024-02-21 12:54:36 +0100

    Lower log level of conf writability warning

--- src/basic/fs-util.c
+++ src/basic/fs-util.c
@@ -311,7 +311,7 @@ int fd_warn_permissions(const char *path, int fd) {
                 log_warning("Configuration file %s is marked executable. Please remove executable permission bits. Proceeding anyway.", path);
 
         if (st.st_mode & 0002)
-                log_warning("Configuration file %s is marked world-writable. Please remove world writability permission bits. Proceeding anyway.", path);
+                log_debug("Configuration file %s is marked world-writable. Please remove world writability permission bits. Proceeding anyway.", path);
 
         if (getpid_cached() == 1 && (st.st_mode & 0044) != 0044)
                 log_warning("Configuration file %s is marked world-inaccessible. This has no effect as configuration data is accessible via APIs without restrictions. Proceeding anyway.", path);
